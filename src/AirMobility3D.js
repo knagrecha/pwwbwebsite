9d102a0b9fdd201f66e4e9ac854784eb9abf5974
Pratyush Muthukumar <30813477+PannuMuthu@users.noreply.github.com>
2023-09-12 00:58:22 -0700

    add hybrid 3d basemap

--- src/AirMobility3D.js
+++ src/AirMobility3D.js
@@ -11,7 +11,7 @@ class AirMobilityMap3D extends React.Component {
       <div>
         <center>
           <iframe
-            src="https://usctrojan.maps.arcgis.com/apps/instant/3dviewer/index.html?appid=fb333feb65bf4a9d955e067fbaf1c183"
+            src="https://stanford.maps.arcgis.com/apps/instant/3dviewer/index.html?appid=67508f88e5284583b7ee847da9693e4e"
             width="1400"
             height="800"
             frameBorder="0"
