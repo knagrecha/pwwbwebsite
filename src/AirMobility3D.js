3e2623ca46097e6ffb25c3028e353932acc69464
Pratyush Muthukumar <30813477+PannuMuthu@users.noreply.github.com>
2024-01-17 17:40:33 -0800

    revert

--- src/AirMobility3D.js
+++ src/AirMobility3D.js
@@ -11,7 +11,7 @@ class AirMobilityMap3D extends React.Component {
       <div>
         <center>
           <iframe
-            src="https://pwwb-digital-twin-5b49ae1d361e.herokuapp.com/"
+            src="https://stanford.maps.arcgis.com/apps/instant/3dviewer/index.html?appid=67508f88e5284583b7ee847da9693e4e"
             width="1400"
             height="800"
             frameBorder="0"
