eef14c4637922ea4f889271481ac789240fd9f05
Shaurya Pathak <47699642+shaurya-pathak@users.noreply.github.com>
2024-09-27 09:30:42 -0700

    Create FireTwin3D.js

---
+++ src/FireTwin3D.js
@@ -0,0 +1,31 @@
+// import React, { Component } from 'react';
+// import { loadModules } from 'esri-loader';
+
+// // https://arcg.is/uuKT9
+
+import React from 'react';
+
+class FireTwinMap3D extends React.Component {
+  render() {
+    return (
+      <div>
+        <center>
+          
+          <iframe
+            src="https://digital-twin-fire-6167eb8a3b8b.herokuapp.com/"
+            width="1400"
+            height="800"
+            frameBorder="0"
+            style={{ border: 0, margin: 0 }}
+            allowFullScreen
+          >
+            iFrames are not supported on this page.
+          </iframe>
+        </center>
+      </div>
+    );
+  }
+}
+
+
+export default AirMobilityMap3D;
